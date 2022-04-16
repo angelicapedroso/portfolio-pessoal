45f3ff02706afac49c84f3e2997040eba88646dc
angelicapedroso <angelicapedroso05@gmail.com>
2022-04-16 15:35:19 -0300

    adiciona informacoes no footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { MdOutlineCopyright } from 'react-icons/md';
 
 export default function Footer() {
   return (
     <div>
-      <footer />
+      <footer>
+        Angélica Pedroso <MdOutlineCopyright /> 2022 - Todos os direitos reservados
+      </footer>
     </div>
   );
 }
