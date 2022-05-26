66aa54ff3c3d68df2edeef80642e2f1e2dbbb12c
angelicapedroso <angelicapedroso05@gmail.com>
2022-05-26 19:57:59 -0300

    refactor: add folder structure

--- portfolio/src/pages/Contact/index.js
+++ portfolio/src/pages/Contact/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/Header';
+import Header from '../../components/Header';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { SiGmail } from 'react-icons/si';
 import { BsWhatsapp } from 'react-icons/bs';
--- portfolio/src/pages/Home/index.js
+++ portfolio/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/Header';
+import Header from '../../components/Header';
 import minhaFoto from '../assets/foto-angelica.jpg';
 
 export default function Home() {
