7424c6e2f5123429874240b9fa31ad5f5cef7aae
angelicapedroso <angelicapedroso05@gmail.com>
2022-05-26 19:29:35 -0300

    faz alteracao da foto

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
-import minhaFoto from '../assets/minhaFoto.jpg';
+import minhaFoto from '../assets/foto-angelica.jpg';
 
 export default function Home() {
   return (
