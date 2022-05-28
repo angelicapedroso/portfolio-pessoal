e4fec6c1088b97cb9b567d819292053200235d64
angelicapedroso <angelicapedroso05@gmail.com>
2022-05-27 22:46:44 -0300

    feat: add css in home page

--- portfolio/src/pages/Home/index.js
+++ portfolio/src/pages/Home/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import Header from '../../components/Header';
-import minhaFoto from '../assets/foto-angelica.jpg';
+import minhaFoto from '../../assets/foto-angelica.png';
+import { BsCodeSlash } from 'react-icons/bs';
+import './styles.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
       <Header />
-      <h1>Angelica Pedroso</h1>
-      <h2>Estudante de Desenvolvimento Web Full-Stack</h2>
-      <figure>
-        <img
-          style={ { width: 200 } }
-          src={ minhaFoto }
-          alt='angelica-pedroso'
-        />
-      </figure>
+      <div className="content">
+        <div className="text">
+          <h1>Angélica Pedroso</h1>
+          <h2>Estudante de Desenvolvimento Web Full-Stack</h2>
+          <BsCodeSlash style={ { fontSize: '40px' } }/>
+        </div>
+        <figure>
+          <img
+            src={ minhaFoto }
+            alt='angelica-pedroso'
+          />
+        </figure>
+      </div>
     </div>
-
   );
 }
