0f9cd41e736de6dc88d94b8881a05cbbd1c03ede
angelicapedroso <angelicapedroso05@gmail.com>
2022-05-30 23:00:29 -0300

    feat: add css in contact page

--- portfolio/src/pages/Contact/index.js
+++ portfolio/src/pages/Contact/index.js
@@ -3,42 +3,45 @@ import Header from '../../components/Header';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { SiGmail } from 'react-icons/si';
 import { BsWhatsapp } from 'react-icons/bs';
+import './styles.css';
 
 export default function Contact() {
   return (
     <div>
       <Header />
-      <h1>Contato</h1>
-      <nav>
-        <a href='https://github.com/angelicapedroso'
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaGithub style={ { fontSize: '40px' } } />
-          angelicapedroso
-        </a>
-        <a href='https://www.linkedin.com/in/angelicapedroso'
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaLinkedin style={ { fontSize: '40px' } }/>
-          Angélica Pedroso
-        </a>
-        <a href='mailto:contatoangelicapedroso@gmail.com'
-          target="_blank"
-          rel="noreferrer"
-        > 
-          <SiGmail style={ { fontSize: '40px' } }/>
-          contatoangelicapedroso@gmail.com
-        </a>
-        <a href='https://wa.me/message/P4Q2PCD52SGBP1'
-          target="_blank"
-          rel="noreferrer"
-        > 
-          <BsWhatsapp style={ { fontSize: '40px' } }/>
-          (33) 999766586
-        </a>
-      </nav>
+      <div className="contact">
+        <h1>Contato</h1>
+        <nav className="contact_container">
+          <a href='https://github.com/angelicapedroso'
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub style={ { fontSize: '40px' } } />
+            angelicapedroso
+          </a>
+          <a href='https://www.linkedin.com/in/angelicapedroso'
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin style={ { fontSize: '40px' } } />
+            Angélica Pedroso
+          </a>
+          <a href='mailto:contatoangelicapedroso@gmail.com'
+            target="_blank"
+            rel="noreferrer"
+          >
+            <SiGmail style={ { fontSize: '40px' } } />
+            contatoangelicapedroso@gmail.com
+          </a>
+          <a href='https://wa.me/message/P4Q2PCD52SGBP1'
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsWhatsapp style={ { fontSize: '40px' } } />
+            (33) 999766586
+          </a>
+        </nav>
+      </div>
     </div>
   );
 }
