452702c8ad3f62c51d159068579165682caecccb
angelicapedroso <angelicapedroso05@gmail.com>
2022-06-01 23:55:04 -0300

    refactor: adjust photo in about page

--- portfolio/src/pages/About/index.js
+++ portfolio/src/pages/About/index.js
@@ -8,15 +8,17 @@ export default function About() {
     <div>
       <Header />
       <div className="about">
-        <h1>Sobre Mim...</h1>
-        <p>Lorem ipsum dolor sit amet. Aut earum pariatur 33 officia architecto ut natus velit eum quasi debitis non consequatur quia nam impedit perferendis est repellendus architecto. Ut veritatis enim qui omnis laudantium est consequuntur officiis. </p><p>Quo autem voluptas eum quam laudantium sit tempora veniam non assumenda laborum sit quidem dolorum At placeat velit in aspernatur commodi. Et quos beatae sed nesciunt maxime vel quaerat voluptates qui laboriosam minima sit eaque quas non quia magnam qui rerum consequatur? </p><p>Qui officiis rerum At voluptates neque et accusantium voluptatem ut architecto minus hic unde dicta cum quisquam doloremque non excepturi incidunt. Quo dolorum tenetur ut nostrum asperiores qui cupiditate natus ad optio exercitationem vel ducimus tempora. Quia sunt quo quia corporis et possimus doloribus est laudantium eveniet cum doloremque iure qui dignissimos sint. Ex voluptatibus consequuntur At illo facilis sit cupiditate fugiat qui repellendus dignissimos et fugiat internos id autem minima At quisquam atque. </p>
+        <div className="text-container">
+          <h1>Sobre Mim...</h1>
+          <p>Lorem ipsum dolor sit amet. Aut earum pariatur 33 officia architecto ut natus velit eum quasi debitis non consequatur quia nam impedit perferendis est repellendus architecto. Ut veritatis enim qui omnis laudantium est consequuntur officiis. </p><p>Quo autem voluptas eum quam laudantium sit tempora veniam non assumenda laborum sit quidem dolorum At placeat velit in aspernatur commodi. Et quos beatae sed nesciunt maxime vel quaerat voluptates qui laboriosam minima sit eaque quas non quia magnam qui rerum consequatur? </p><p>Qui officiis rerum At voluptates neque et accusantium voluptatem ut architecto minus hic unde dicta cum quisquam doloremque non excepturi incidunt. Quo dolorum tenetur ut nostrum asperiores qui cupiditate natus ad optio exercitationem vel ducimus tempora. Quia sunt quo quia corporis et possimus doloribus est laudantium eveniet cum doloremque iure qui dignissimos sint. Ex voluptatibus consequuntur At illo facilis sit cupiditate fugiat qui repellendus dignissimos et fugiat internos id autem minima At quisquam atque. </p>
+        </div>
+        <figure>
+          <img
+            src={ minhaFoto }
+            alt='angelica-pedroso'
+          />
+        </figure>
       </div>
-      <figure>
-        <img
-          src={ minhaFoto }
-          alt='angelica-pedroso'
-        />
-      </figure>
     </div>
   );
 }
